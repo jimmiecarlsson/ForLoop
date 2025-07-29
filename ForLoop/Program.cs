d215da70a57f52bb344359a7e0e9270e31fa3824
jimmiecarlsson <jimpa.c66@gmail.com>
2025-07-29 14:24:11 +0200

    Done exercise

--- ForLoop/Program.cs
+++ ForLoop/Program.cs
@@ -1,5 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// Exercise 3.a
+Console.WriteLine("--------------- Exercise 3.a ----------------");
 
+for (int i = 1; i < 6; i++)
+{
+    Console.WriteLine("B" + i);
+}
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("--------------- Exercise 3.b ----------------");
+
+for (int i = 7;i < 36; i = i + 7)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("--------------- Exercise 3.c ----------------");
+
+string strBees = "";
+
+for (int i = 4; i < 9; i++)
+{
+    strBees = strBees + "B";
+    Console.WriteLine( strBees + i);
+}
