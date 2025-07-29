f3b6e42e7e6a975c23302a3fdac3e5b94d0b49b5
jimmiecarlsson <jimpa.c66@gmail.com>
2025-07-29 15:25:56 +0200

    Add the old version commented

--- ForLoop/Program.cs
+++ ForLoop/Program.cs
@@ -1,28 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
+//// Exercise 3.a
+//Console.WriteLine("--------------- Exercise 3.a ----------------");
+
+//for (int i = 1; i < 6; i++)
+//{
+//    Console.WriteLine("B" + i);
+//}
+
+//// Exercise 3.b
+//Console.WriteLine("--------------- Exercise 3.b ----------------");
+
+//for (int j = 7; j < 36; j = j +7)
+//{
+//    Console.WriteLine(j);
+//}
+
+//// Exercise 3.c
+//Console.WriteLine("--------------- Exercise 3.c ----------------");
+
+//string strBees = "";
+
+//for (int i = 4; i < 9; i++)
+//{
+//    strBees = strBees + "B";
+//    Console.WriteLine(strBees + i);
+//}
+
+
+// Förenklat
+
 // Exercise 3.a
 Console.WriteLine("--------------- Exercise 3.a ----------------");
 
 for (int i = 1; i < 6; i++)
     Console.WriteLine("B" + i);
 
-    Console.WriteLine("--------------- Exercise 3.b ----------------");
+// Exercise 3.b
+Console.WriteLine("--------------- Exercise 3.b ----------------");
 
 for (int j = 7; j < 36; j = j + 7)
     Console.WriteLine(j);
 
-
-    Console.WriteLine("--------------- Exercise 3.c ----------------");
-
-    //string strBees = "";
-
-    //for (int i = 4; i < 9; i++)
-    //{
-    //    strBees = strBees + "B";
-    //    Console.WriteLine( strBees + i);
-    //}
-
-    // Förenklar
+// Exercise 3.c
+Console.WriteLine("--------------- Exercise 3.c ----------------");
 
 for (int a = 4, b = 0; a < 9; a++, b++)
     Console.WriteLine(new string('B', b) + a);
